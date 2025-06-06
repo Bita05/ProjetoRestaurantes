8cc1215d82f07021ee1aeed61a4deb1a8e1a3a0d
Bita05 <brunombita@gmail.com>
2025-06-06 23:47:46 +0100

    update

--- Server/src/config/db.php
+++ Server/src/config/db.php
@@ -1,10 +1,10 @@
 <?php
 // Pega as variáveis de ambiente definidas no Render
-$host = getenv('DB_HOST');        // ex: hopper.proxy.rlwy.net
-$port = getenv('DB_PORT');        // ex: 15790
-$dbname = getenv('DB_NAME');      // ex: railway
-$username = getenv('DB_USER');    // ex: root
-$password = getenv('DB_PASSWORD'); // a password da base
+$host = getenv('DATABASE_HOST');        // ex: hopper.proxy.rlwy.net
+$port = getenv('DATABASE_PORT');        // ex: 15790
+$dbname = getenv('DATABASE_NAME');      // ex: railway
+$username = getenv('DATABASE_USER');    // ex: root
+$password = getenv('DATABASE_PASSWORD'); // a password da base
 
 // Cria o DSN incluindo o host, porta e base de dados
 $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
