343aea95304d2c0a7e249144e5d2c1c183a0666b
Bruno Bita <58052558+Bita05@users.noreply.github.com>
2025-06-10 01:44:43 +0100

    Update home.jsx

--- projetorestaurantes/src/components/clientes/home.jsx
+++ projetorestaurantes/src/components/clientes/home.jsx
@@ -54,7 +54,7 @@ const [snackbarSeverity, setSnackbarSeverity] = useState('info'); // 'success', 
             });
 
 
-    fetch('https://projetorestaurantes.onrender.com/restaurante//RestaurantesPopulares')
+    fetch('https://projetorestaurantes.onrender.com/restaurante/RestaurantesPopulares')
     .then(response => response.json())
     .then(data => {
         if (data.status === 'success') {
@@ -105,7 +105,7 @@ const [snackbarSeverity, setSnackbarSeverity] = useState('info'); // 'success', 
                     return;
                 }
     
-                const horariosData = await fetch('http://localhost:8080/horariosReserva', {
+                const horariosData = await fetch('https://projetorestaurantes.onrender.com/horariosReserva', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({
@@ -133,7 +133,7 @@ const [snackbarSeverity, setSnackbarSeverity] = useState('info'); // 'success', 
                     return;
                 }
     
-                const menuData = await fetch('http://localhost:8080/restaurante/listarMenu', {
+                const menuData = await fetch('https://projetorestaurantes.onrender.com/restaurante/listarMenu', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ id_restaurante: selectedRestaurant.id_restaurante })
@@ -272,7 +272,7 @@ const [snackbarSeverity, setSnackbarSeverity] = useState('info'); // 'success', 
         const idRestaurante = selectedRestaurant?.id_restaurante;
     
         try {
-            const response = await fetch('http://localhost:8080/verificarReservasExistentes', {
+            const response = await fetch('https://projetorestaurantes.onrender.com/verificarReservasExistentes', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
